a717d96cdc1ecee7e290a9b4859d9c931777bada
dtimofeev <td090833@gmail.com>
2023-03-11 13:02:45 +0200

    Added types and fixed fallen tests

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,5 +1,7 @@
 import request from 'supertest';
 import {app, HTTP_STATUSES} from "../source";
+import {CreateCourseModel} from "../source/models/CreateCourseModel";
+import {UpdateCourseModel} from "../source/models/UpdateCourseModel";
 describe('/course' ,() => {
   beforeAll(async () => {
     await request(app).delete('/__test__/data/');
@@ -11,29 +13,48 @@ describe('/course' ,() => {
     await request(app).get('/courses/999').expect(HTTP_STATUSES.NOT_FOUND_404);
   });
   it('shouldn`t create a course with incorrect input data' , async() =>{
-    await request(app).post('/courses').send({title : ''})
+    const data : CreateCourseModel = {title : ''};
+    await request(app).post('/courses').send(data)
       .expect(HTTP_STATUSES.BAD_REQUEST_400);
     await request(app).get('/courses').expect(HTTP_STATUSES.SUCCESS_200,[]);
   });
   let createdCourse : any = null;
   it('should create a course with correct input data' , async() =>{
+    const data : CreateCourseModel = {title : 'dario-rerio course'};
     const createResponse = await request(app)
       .post('/courses')
-      .send({title : 'dario-rerio course'})
+      .send(data)
       .expect(HTTP_STATUSES.CREATED_201);
     createdCourse = createResponse.body;
     expect(createdCourse).toEqual({
       id : expect.any(Number),
-      title : 'dario-rerio course',
+      title : data.title,
     })
     await request(app)
       .get('/courses/')
       .expect(HTTP_STATUSES.SUCCESS_200,[createdCourse]);
   });
+  let anotherCourse : any = null;
+  it('create another course' , async() =>{
+    const data : CreateCourseModel = {title : 'dario-rerio course 2'};
+    const createResponse = await request(app)
+      .post('/courses')
+      .send(data)
+      .expect(HTTP_STATUSES.CREATED_201);
+    anotherCourse = createResponse.body;
+    expect(anotherCourse).toEqual({
+      id : expect.any(Number),
+      title : data.title,
+    })
+    await request(app)
+      .get('/courses')
+      .expect(HTTP_STATUSES.SUCCESS_200,[createdCourse, anotherCourse]);
+  });
   it('shouldn`t update a course with incorrect input data' , async() =>{
+    const data : CreateCourseModel = {title : ''};
     await request(app)
       .put('/courses/' + createdCourse.id)
-      .send({title : ''})
+      .send(data)
       .expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(createdCourse).toEqual({
@@ -44,49 +65,38 @@ describe('/course' ,() => {
       .get('/courses/' + createdCourse.id)
       .expect(HTTP_STATUSES.SUCCESS_200,createdCourse);
   });
-  let anotherCourse : any = null;
+  //let anotherCourse : any = null;
   it('shouldn`t update a course that does not exist ' , async() =>{
     await request(app)
       .put('/courses/' + -100)
       .send({title : 'good'})
       .expect(HTTP_STATUSES.NOT_FOUND_404);
 
-    expect(createdCourse).toEqual({
+   expect(createdCourse).toEqual({
       id : expect.any(Number),
       title : 'dario-rerio course',
     })
+
+
     await request(app)
       .get('/courses/' + createdCourse.id)
       .expect(HTTP_STATUSES.SUCCESS_200,createdCourse);
   });
   it('should update a course with correct input data ' , async() =>{
+    const data : UpdateCourseModel = {title : 'good new title'};
     await request(app)
       .put('/courses/' + createdCourse.id)
-      .send({title : 'good new title'})
+      .send(data)
       .expect(HTTP_STATUSES.NO_CONTENT_204);
     await request(app)
       .get('/courses/' + createdCourse.id)
       .expect(HTTP_STATUSES.SUCCESS_200, {
         ...createdCourse,
-        title : 'good new title',
+        title : data.title,
       });
     await request(app)
       .get('/courses/' + anotherCourse.id)
       .expect(HTTP_STATUSES.SUCCESS_200, anotherCourse);
-  });
-  it('create another course' , async() =>{
-    const createResponse = await request(app)
-      .post('/courses')
-      .send({title : 'dario-rerio course 2'})
-      .expect(HTTP_STATUSES.CREATED_201);
-    anotherCourse = createResponse.body;
-    expect(anotherCourse).toEqual({
-      id : expect.any(Number),
-      title : 'dario-rerio course 2',
-    })
-    await request(app)
-      .get('/courses')
-      .expect(HTTP_STATUSES.SUCCESS_200,[createdCourse, anotherCourse]);
   });
   it('should delete both courses ' , async() =>{
     await request(app)
